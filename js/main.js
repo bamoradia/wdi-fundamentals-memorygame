9eef1349a6bee79064a5a3d1fd5440b2f691f24c
Badal <badalmoradia@gmail.com>
2018-06-06 15:55:22 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,40 @@
-var cards = ['queen', 'queen', 'king', 'king'];
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}]
 var cardsInPlay = [];
 
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]){
-			alert("Your found a match!");
-		} else {
+			alert("You found a match!");
+	} 
+	else {
 			alert("Sorry, try again.");
-		}
+	}
 }
 
 var flipCard = function (cardID) {
-	console.log("User flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("User flipped a " + cards[cardID].rank);
+	cardsInPlay.push(cards[cardID].rank);
+	console.log(cards[cardID].suit);
+	console.log(cards[cardID].cardImage);
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}	
